08413a590ba12310919e5cd8294b9d765de6a6c8
Myusuf05 <ymnpsc@gmail.com>
2022-01-23 18:21:06 +0400

    updates 06200023012022

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -13,8 +13,8 @@ export const isWinningWord = (word: string) => {
 }
 
 export const getWordOfDay = () => {
-  // January 1, 2022 Game Epoch
-  const epochMs = 1641013200000
+  // January 22, 2022 Game Epoch
+  const epochMs = 1642809600000
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
